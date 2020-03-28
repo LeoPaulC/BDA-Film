ae99905332935eb5317eca390c5918c8847bb36d
TRAORE paul <traore@informatique.univ-paris-diderot.fr>
2020-03-28 17:27:22 +0100

    primary key

--- tables.sql
+++ tables.sql
@@ -4,22 +4,20 @@ CREATE TABLE Personne(
 		prenom_personne VARCHAR(50) NOT NULL,
 		date_naissance DATE
 );
-CREATE TABLE Personne_Invitée(
+CREATE TABLE Personne_Invitee(
 		Id_personne INTEGER ,
 		Id_film INTEGER ,
 		Id_festival INTEGER ,
 		metier VARCHAR(50) NOT NULL,
-		date_arrivee DATE,
-		date_depart DATE ,
-		PRIMARY KEY(Id_personne , Id_film , Id_festival ) ,
-		FOREIGN KEY(Id_personne) REFERENCES Personne(Id_personne) ,
-		FOREIGN KEY(Id_film) REFERENCES Film(Id_film) ,
-		FOREIGN KEY(Id_festival) REFERENCES Festival(Id_festival) ,
+		PRIMARY KEY(Id_personne, Id_film, Id_festival),
+		FOREIGN KEY(Id_personne) REFERENCES Personne(Id_personne),
+		FOREIGN KEY(Id_film) REFERENCES Film(Id_film),
+		FOREIGN KEY(Id_festival) REFERENCES Festival(Id_festival)
 );
 CREATE TABLE Film(
 		Id_film INTEGER PRIMARY KEY,
 		nom_film VARCHAR(50) NOT NULL,
-		type_film VARCHAR(50) NOT NULL,
+		type_film VARCHAR(50),
 		duree_film INTEGER,
 		Date_parution DATE
 );
@@ -30,59 +28,62 @@ CREATE TABLE Participant(
 		Role VARCHAR(50) NOT NULL
 );
 CREATE TABLE Utilisateur(
-		Id_user INTEGER PRIMARY KEY,
-		Id_personne INTEGER PRIMARY KEY,
-		Id_localisation INTEGER PRIMARY KEY,
+		Id_user INTEGER,
+		Id_personne INTEGER,
+		Id_localisation INTEGER,
 		Email VARCHAR(50) NOT NULL,
 		Mdp VARCHAR(50) NOT NULL,
-		Téléphone 
+		Telephone VARCHAR(50),
+		PRIMARY KEY(Id_user, Id_personne, Id_localisation)
 );
 CREATE TABLE Localisation(
 		Id_localisation INTEGER PRIMARY KEY,
-		Latitude INTEGER PRIMARY KEY,
-		Longitude INTEGER PRIMARY KEY
+		Latitude INTEGER,
+		Longitude INTEGER
 );
 CREATE TABLE Lieu(
-		Latitude INTEGER PRIMARY KEY,
-		Longitude INTEGER PRIMARY KEY,
+		Latitude INTEGER,
+		Longitude INTEGER,
 		Departement VARCHAR(50) NOT NULL,
-		Ville VARCHAR(50) NOT NULL,
+		Ville VARCHAR(50),
 		Pays VARCHAR(50) NOT NULL,
-		Adresse VARCHAR(50) NOT NULL,
-		Code_postale INTEGER
+		Adresse VARCHAR(50),
+		Code_postale VARCHAR(6) NOT NULL,
+		PRIMARY KEY(Latitude, Longitude)
 );
 CREATE TABLE Festival(
 		Id_festival INTEGER PRIMARY KEY,
-		Id_edition INTEGER PRIMARY KEY,
-		Theme VARCHAR(50) NOT NULL,
+		Theme VARCHAR(50),
 		Nom_festival VARCHAR(50) NOT NULL,
 		Capacite_max INTEGER,
-		Description_lieu VARCHAR(50) NOT NULL,
-		Periodicite VARCHAR(50) NOT NULL,
-		Date_création DATE,
-		Website VARCHAR(50) NOT NULL
+		Description_lieu VARCHAR(50),
+		Periodicite VARCHAR(50),
+		Date_creation DATE,
+		Website VARCHAR(50)
 );
 CREATE TABLE Edition(
-		Id_edition INTEGER PRIMARY KEY,
-		Id_festival INTEGER PRIMARY KEY,
-		Année PRIMARY KEY,
+		Id_edition INTEGER,
+		Id_festival INTEGER,
+		Annee INTEGER,
 		Date_debut DATE,
 		Date_fin DATE,
-		Id_localisation INTEGER
+		Id_localisation INTEGER,
+		PRIMARY KEY(Id_edition, Id_festival, Annee)
 );
 CREATE TABLE Place(
-		Id_place INTEGER PRIMARY KEY,
-		Id_festival INTEGER PRIMARY KEY,
-		Id_personne INTEGER PRIMARY KEY,
+		Id_place INTEGER,
+		Id_festival INTEGER,
+		Id_personne INTEGER,
 		Nom_place VARCHAR(50) NOT NULL,
 		Prenom_place VARCHAR(50) NOT NULL,
-		Id_catégorie INTEGER,
-		Numéro_place INTEGER
+		Id_categorie INTEGER,
+		Numero_place INTEGER,
+		PRIMARY KEY(Id_place, Id_festival, Id_personne)
 );
 CREATE TABLE Categorie(
 		Id_categorie INTEGER PRIMARY KEY,
 		Prix INTEGER,
-		Descriptif VARCHAR(50) NOT NULL
+		Descriptif VARCHAR(50) 
 );
 CREATE TABLE Site_Critique(
 		Id_site INTEGER PRIMARY KEY,
@@ -90,9 +91,10 @@ CREATE TABLE Site_Critique(
 		Lien VARCHAR(50) NOT NULL
 );
 CREATE TABLE Critique(
-		Id_critique INTEGER PRIMARY KEY,
-		Id_film INTEGER PRIMARY KEY,
-		Id_site INTEGER PRIMARY KEY,
+		Id_critique INTEGER,
+		Id_film INTEGER,
+		Id_site INTEGER,
 		Note_global INTEGER,
-		Avis_general VARCHAR(50) NOT NULL
+		Avis_general VARCHAR(50),
+		PRIMARY KEY(Id_critique, Id_film, Id_site)
 );
